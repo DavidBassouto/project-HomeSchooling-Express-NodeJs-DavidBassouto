f523958d7be07aec37a93b2362b817b63a8a3c2e
Debora Correa <deboragomescorr@gmail.com>
2022-09-09 17:08:49 -0300

    feat: function creates guardian session

--- src/routes/guardians.routes.ts
+++ src/routes/guardians.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createGuardianController from "../controllers/guardians/createGuardian.controller";
+import createGuardianSessionController from "../controllers/guardians/createGuardianSession.controller";
 
 const routes = Router();
 
 export const guardianRoutes = () => {
   routes.post("/", createGuardianController);
+  routes.post("/login", createGuardianSessionController);
 
   return routes;
 };
