00da9c9c8ac998904ee439b033c7a58dcd9b7f97
Francisco Jeihson <jeihson_alvesso@hotmail.com>
2022-09-12 20:05:54 -0300

    feat: delete guardian

---
+++ src/controllers/guardians/deleteGuardian.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { AppError } from "../../errors/AppError";
+import deleteGuardianService from "../../services/guardians/deleteGuardian.service";
+
+
+const deleteGuardianController = async (req: Request, res: Response) => {
+
+    const id = req.params.id
+
+    if(!id){
+        throw new AppError(404, "Guardian not found")
+    }
+    
+    const guardianDeleted = await deleteGuardianService(id)
+
+    return res.status(200).json({ message: "Guardian deleted with sucess!" });
+
+}
+
+export default deleteGuardianController
