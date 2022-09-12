77580f7986beba029137e0f98164ce91835041c2
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-09-12 16:35:03 +0100

    feat: update teacher route

---
+++ src/controllers/teachers/teacherUpdate.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { AppError, handleError } from "../../errors/AppError";
+import teacherUpdateService from "../../services/teachers/teacherUpdate.service";
+
+export const teacherUpdateController = async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params
+        const {data} = req.body
+
+        const updatedTeacher = await teacherUpdateService(id, data)
+
+        return res.status(200).send(updatedTeacher)
+
+    } catch (err) {
+        if(err instanceof AppError){
+            handleError(err, res)
+        }
+    }
+}
+
+export default teacherUpdateController
--- src/interfaces/teachers/index.ts
+++ src/interfaces/teachers/index.ts
@@ -10,3 +10,11 @@ export interface ITeacherLogin {
     email: string
     password: string
 }
+
+export interface ITeacherUpdate {
+    name: string
+    email: string
+    password: string
+    subject: string
+    bio: string
+}
